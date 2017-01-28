cc703d641bad9f243a2a0e5bce4a395ade1011e3
TerusLimsurut <few2012few@gmail.com>
2017-01-28 19:16:31 +0700

    Update BOBObot.php

--- BOBObot.php
+++ BOBObot.php
@@ -6,12 +6,12 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 $file = fopen('data_test.csv', 'r');
-//$data_ary=array("")
+$data_ary=array("u")
 //while (($line = fgetcsv($file)) !== FALSE) {
   //$line is an array of the csv elements
 //  $data_ary = array_push($data_ary, $line);
 //}
-//echo $data_ary;
+echo $data_ary;
 fclose($file);
 if (!is_null($events['events'])) {
 	// Loop through each event
