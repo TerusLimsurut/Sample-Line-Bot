a6f79c245f1eb4b462008b84f79157125f53b98d
TerusLimsurut <few2012few@gmail.com>
2017-01-28 21:09:59 +0700

    Update BOBObot.php

--- BOBObot.php
+++ BOBObot.php
@@ -33,10 +33,12 @@ if (!is_null($events['events'])) {
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			if ($messages==($data_ary[0])[0]){
+			//($data_ary[0])[0])
+			//[($data_ary[0])[1]
+			if ($messages=="a"){
 				$data = [
 					'replyToken' => $replyToken,
-					'messages' => [($data_ary[0])[1],
+					'messages' => ["b"],
 				];
 			}
 			$post = json_encode($data);
