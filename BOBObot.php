902562e8b3032b537162b18c6764af84a31c5976
TerusLimsurut <few2012few@gmail.com>
2017-01-29 12:34:50 +0700

    Update BOBObot.php

--- BOBObot.php
+++ BOBObot.php
@@ -24,57 +24,57 @@ while (($line = fgetcsv($file)) !== FALSE) {
 fclose($file);
 
 
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			//($data_ary[0])[0])   $data_ary[$j][0])
-			//[($data_ary[0])[1]
+// if (!is_null($events['events'])) {
+// 	// Loop through each event
+// 	foreach ($events['events'] as $event) {
+// 		// Reply only when message sent is in 'text' format
+// 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+// 			// Get text sent
+// 			$text = $event['message']['text'];
+// 			// Get replyToken
+// 			$replyToken = $event['replyToken'];
+// 			// Build message to reply back
+// 			$messages = [
+// 				'type' => 'text',
+// 				'text' => $text
+// 			];
+// 			// Make a POST Request to Messaging API to reply to sender
+// 			$url = 'https://api.line.me/v2/bot/message/reply';
+// 			//($data_ary[0])[0])   $data_ary[$j][0])
+// 			//[($data_ary[0])[1]
 
-      try {
-          $messages = [
-            'type' => 'text',
-            'text' => array_rand($data_ary[$text], 1);
-          ];
-          $data = [
-            'replyToken' => $replyToken,
-            'messages' => [$messages],
-          ];
-      } catch (Exception $e) {
-          $messages = [
-            'type' => 'text',
-            'text' => "สอนฉันหน่อย"
-          ];
-          $data = [
-            'replyToken' => $replyToken,
-            'messages' => [$messages],
-          ];
-      }
+//       try {
+//           $messages = [
+//             'type' => 'text',
+//             'text' => array_rand($data_ary[$text], 1);
+//           ];
+//           $data = [
+//             'replyToken' => $replyToken,
+//             'messages' => [$messages],
+//           ];
+//       } catch (Exception $e) {
+//           $messages = [
+//             'type' => 'text',
+//             'text' => "สอนฉันหน่อย"
+//           ];
+//           $data = [
+//             'replyToken' => $replyToken,
+//             'messages' => [$messages],
+//           ];
+//       }
       
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			echo $result . "\r\n";
-		}
-	}
-}
+// 			$post = json_encode($data);
+// 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+// 			$ch = curl_init($url);
+// 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+// 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+// 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// 			$result = curl_exec($ch);
+// 			curl_close($ch);
+// 			echo $result . "\r\n";
+// 		}
+// 	}
+// }
 echo "OK";
