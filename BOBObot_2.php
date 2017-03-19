ce5787b11c0977482544646a0dc10dba7a158ff6
TerusLimsurut <few2012few@gmail.com>
2017-03-19 17:46:28 +0700

    .

--- BOBObot_2.php
+++ BOBObot_2.php
@@ -1,5 +1,5 @@
 <?php
-$myfile = fopen("Log_chat_2.txt", "w") or die("Unable to open file!");
+$myfile = fopen("Log_chat_2_2.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 $txt = "Jane Doe\n";
